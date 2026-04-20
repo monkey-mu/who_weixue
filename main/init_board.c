2d0f2442d56b378e5c7abdda783c1ef0c69f8449
木盆 <16476573+wooden-basin@user.noreply.gitee.com>
2026-04-20 16:02:47 +0800

    初始化独立

---
+++ main/init_board.c
@@ -0,0 +1,327 @@
+#include "init_board.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_touch_cst816s.h"
+#include "lvgl.h"
+#include "esp_camera.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+static const char *TAG = "init_board";
+
+// 全局变量定义
+lv_disp_drv_t disp_drv;
+esp_lcd_panel_handle_t panel_handle;
+esp_lcd_touch_handle_t tp;
+
+static SemaphoreHandle_t lvgl_api_mux = NULL;
+
+bool lvgl_lock(int timeout_ms)
+{
+    // Convert timeout in milliseconds to FreeRTOS ticks
+    // If `timeout_ms` is set to -1, the program will block until the condition is met
+    const TickType_t timeout_ticks = (timeout_ms == -1) ? portMAX_DELAY : pdMS_TO_TICKS(timeout_ms);
+    return xSemaphoreTakeRecursive(lvgl_api_mux, timeout_ticks) == pdTRUE;
+}
+
+void lvgl_unlock(void)
+{
+    xSemaphoreGiveRecursive(lvgl_api_mux);
+}
+
+static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_disp_flush_ready(&disp_drv);
+    return false;
+}
+
+static void example_increase_lvgl_tick(void *arg)
+{
+    /* Tell LVGL how many milliseconds has elapsed */
+    lv_tick_inc(EXAMPLE_LVGL_TICK_PERIOD_MS);
+}
+
+static void example_lvgl_flush_cb(lv_disp_drv_t *drv, const lv_area_t *area, lv_color_t *color_map)
+{
+    int offsetx1 = area->x1;
+    int offsetx2 = area->x2;
+    int offsety1 = area->y1;
+    int offsety2 = area->y2;
+    // copy a buffer's content to a specific area of the display
+
+    esp_lcd_panel_draw_bitmap(panel_handle, offsetx1, offsety1, offsetx2 + 1, offsety2 + 1, color_map);
+}
+
+static void example_lvgl_touch_cb(lv_indev_drv_t *drv, lv_indev_data_t *data)
+{
+    esp_lcd_touch_point_data_t touch_data;
+    uint16_t touchpad_x[1] = {0};
+    uint16_t touchpad_y[1] = {0};
+    uint8_t touchpad_cnt = 0;
+    esp_lcd_touch_read_data(tp);
+    /* Get coordinates */
+    bool touchpad_pressed = esp_lcd_touch_get_coordinates(tp, touchpad_x, touchpad_y, NULL, &touchpad_cnt, 1);
+
+    if (touchpad_pressed && touchpad_cnt > 0)
+    {
+        data->point.x = touchpad_x[0];
+        data->point.y = touchpad_y[0];
+        data->state = LV_INDEV_STATE_PRESSED;
+    }
+    else
+    {
+        data->state = LV_INDEV_STATE_RELEASED;
+    }
+}
+
+void lv_port_disp_init(void)
+{
+    static lv_disp_draw_buf_t draw_buf;
+    lv_color_t *buf1 = heap_caps_malloc(EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES * sizeof(lv_color_t), MALLOC_CAP_SPIRAM);
+    assert(buf1);
+    lv_color_t *buf2 = heap_caps_malloc(EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES * sizeof(lv_color_t), MALLOC_CAP_SPIRAM);
+    assert(buf2);
+    lv_disp_draw_buf_init(&draw_buf, buf1, buf2,
+                          EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES); /*Initialize the display buffer*/
+
+    /*-----------------------------------
+     * Register the display in LVGL
+     *----------------------------------*/
+    lv_disp_drv_init(&disp_drv); /*Basic initialization*/
+
+    /*Set up the functions to access to your display*/
+
+    /*Set the resolution of the display*/
+    disp_drv.hor_res = EXAMPLE_LCD_H_RES;
+    disp_drv.ver_res = EXAMPLE_LCD_V_RES;
+
+    /*Used to copy the buffer's content to the display*/
+    disp_drv.flush_cb = example_lvgl_flush_cb;
+
+    /*Set a display buffer*/
+    disp_drv.draw_buf = &draw_buf;
+
+    /*Required for Example 3)*/
+    disp_drv.full_refresh = 1;
+    // disp_drv.direct_mode = 1;
+   // disp_drv.rotated = 1;
+    /* Fill a memory array with a color if you have GPU.
+     * Note that, in lv_conf.h you can enable GPUs that has built-in support in LVGL.
+     * But if you have a different GPU you can use with this callback.*/
+    // disp_drv.gpu_fill_cb = gpu_fill;
+
+    /*Finally register the driver*/
+    lv_disp_drv_register(&disp_drv);
+}
+
+void lv_port_indev_init(void)
+{
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    // indev_drv.disp = disp;
+    indev_drv.read_cb = example_lvgl_touch_cb;
+    indev_drv.user_data = tp;
+
+    lv_indev_drv_register(&indev_drv);
+}
+
+void display_init(void)
+{
+    ESP_LOGI(TAG, "SPI BUS init");
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = EXAMPLE_PIN_NUM_SCLK,
+        .mosi_io_num = EXAMPLE_PIN_NUM_MOSI,
+        .miso_io_num = EXAMPLE_PIN_NUM_MISO,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 4000,
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(EXAMPLE_SPI_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    ESP_LOGI(TAG, "Install panel IO");
+
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = EXAMPLE_PIN_NUM_LCD_DC,
+        .cs_gpio_num = EXAMPLE_PIN_NUM_LCD_CS,
+        .pclk_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,
+        .lcd_param_bits = EXAMPLE_LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+        .on_color_trans_done = example_notify_lvgl_flush_ready,
+    };
+    // Attach the LCD to the SPI bus
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)EXAMPLE_SPI_HOST, &io_config, &io_handle));
+
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = EXAMPLE_PIN_NUM_LCD_RST,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = 16,
+    };
+    ESP_LOGI(TAG, "Install ST7789 panel driver");
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, false, false));
+    ESP_ERROR_CHECK(esp_lcd_panel_swap_xy(panel_handle, false));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, true));
+}
+
+
+void touch_init(void)
+{
+    /* Initilize I2C */
+    i2c_master_bus_handle_t i2c_handle = NULL;
+    const i2c_master_bus_config_t i2c_config = {
+        .i2c_port = EXAMPLE_I2C_NUM,
+        .sda_io_num = EXAMPLE_PIN_NUM_I2C_SDA,
+        .scl_io_num = EXAMPLE_PIN_NUM_I2C_SCL,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_config, &i2c_handle));
+
+    esp_lcd_touch_config_t tp_cfg = {
+        .x_max = EXAMPLE_LCD_V_RES,
+        .y_max = EXAMPLE_LCD_H_RES,
+        .rst_gpio_num = -1,
+        .int_gpio_num = -1,
+        .levels = {
+            .reset = 0,
+            .interrupt = 0,
+        },
+        .flags = {
+            .swap_xy = 0,
+            .mirror_x = 0,
+            .mirror_y = 0,
+        },
+    };
+
+    esp_lcd_panel_io_handle_t tp_io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t tp_io_config = ESP_LCD_TOUCH_IO_I2C_CST816S_CONFIG();
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_handle, &tp_io_config, &tp_io_handle));
+    ESP_ERROR_CHECK(esp_lcd_touch_new_i2c_cst816s(tp_io_handle, &tp_cfg, &tp));
+    ESP_LOGI(TAG, "Initialize touch controller CST816");
+}
+
+void bsp_brightness_init(void)
+{
+    gpio_set_direction(EXAMPLE_PIN_NUM_BK_LIGHT, GPIO_MODE_OUTPUT);
+    gpio_set_level(EXAMPLE_PIN_NUM_BK_LIGHT, 1);
+
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode = LCD_BL_LEDC_MODE,
+        .timer_num = LCD_BL_LEDC_TIMER,
+        .duty_resolution = LCD_BL_LEDC_DUTY_RES,
+        .freq_hz = LCD_BL_LEDC_FREQUENCY, // Set output frequency at 5 kHz
+        .clk_cfg = LEDC_AUTO_CLK};
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode = LCD_BL_LEDC_MODE,
+        .channel = LCD_BL_LEDC_CHANNEL,
+        .timer_sel = LCD_BL_LEDC_TIMER,
+        .intr_type = LEDC_INTR_DISABLE,
+        .gpio_num = EXAMPLE_PIN_NUM_BK_LIGHT,
+        .duty = 0, // Set duty to 0%
+        .hpoint = 0};
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+void bsp_brightness_set_level(uint8_t level)
+{
+    if (level > 100)
+    {
+        ESP_LOGE(TAG, "Brightness value out of range");
+        return;
+    }
+
+    uint32_t duty = (level * (LCD_BL_LEDC_DUTY - 1)) / 100;
+
+    ESP_ERROR_CHECK(ledc_set_duty(LCD_BL_LEDC_MODE, LCD_BL_LEDC_CHANNEL, duty));
+    ESP_ERROR_CHECK(ledc_update_duty(LCD_BL_LEDC_MODE, LCD_BL_LEDC_CHANNEL));
+
+    ESP_LOGI(TAG, "LCD brightness set to %d%%", level);
+}
+
+void lvgl_tick_timer_init(uint32_t ms)
+{
+    ESP_LOGI(TAG, "Install LVGL tick timer");
+    // Tick interface for LVGL (using esp_timer to generate 2ms periodic event)
+    const esp_timer_create_args_t lvgl_tick_timer_args = {
+        .callback = &example_increase_lvgl_tick,
+        .name = "lvgl_tick"};
+    esp_timer_handle_t lvgl_tick_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, ms * 1000));
+}
+
+void camera_init(void)
+{
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_1;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
+    config.pin_sccb_sda = SIOD_GPIO_NUM;
+    config.pin_sccb_scl = SIOC_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
+    config.xclk_freq_hz = 20000000;
+    config.frame_size = FRAMESIZE_HQVGA;
+    config.pixel_format = PIXFORMAT_RGB565; // for streaming
+    // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+    config.grab_mode = CAMERA_GRAB_LATEST;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 10;
+    config.fb_count = 1;
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+        printf("Camera init failed with error 0x%x", err);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    s->set_hmirror(s, 1);
+}
+
+void downscale_rgb565(
+    uint16_t *src, int src_w, int src_h,
+    uint16_t *dst, int dst_w, int dst_h)
+{
+    int step_x = src_w / dst_w;
+    int step_y = src_h / dst_h;
+
+    for (int y = 0; y < dst_h; y++) {
+        int src_y = y * step_y;
+        for (int x = 0; x < dst_w; x++) {
+            int src_x = x * step_x;
+
+            dst[y * dst_w + x] =
+                src[src_y * src_w + src_x];
+        }
+    }
+}
---
+++ main/init_board.h
@@ -0,0 +1,103 @@
+#ifndef INIT_BOARD_H
+#define INIT_BOARD_H
+
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_touch_cst816s.h"
+
+#include "driver/spi_master.h"
+#include "driver/gpio.h"
+#include "driver/ledc.h"
+#include "driver/i2c_master.h"
+
+#include "lvgl.h"
+
+#include "esp_camera.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// 引脚定义
+#define PWDN_GPIO_NUM 17  // power down is not used
+#define RESET_GPIO_NUM -1 // software reset will be performed
+#define XCLK_GPIO_NUM 8
+#define SIOD_GPIO_NUM 21
+#define SIOC_GPIO_NUM 16
+
+#define Y9_GPIO_NUM 2
+#define Y8_GPIO_NUM 7
+#define Y7_GPIO_NUM 10
+#define Y6_GPIO_NUM 14
+#define Y5_GPIO_NUM 11
+#define Y4_GPIO_NUM 15
+#define Y3_GPIO_NUM 13
+#define Y2_GPIO_NUM 12
+#define VSYNC_GPIO_NUM 6
+#define HREF_GPIO_NUM 4
+#define PCLK_GPIO_NUM 9
+
+#define EXAMPLE_PIN_NUM_SCLK 39
+#define EXAMPLE_PIN_NUM_MOSI 38
+#define EXAMPLE_PIN_NUM_MISO 40
+
+#define EXAMPLE_SPI_HOST SPI2_HOST
+
+#define EXAMPLE_I2C_NUM 0 // I2C number
+#define EXAMPLE_PIN_NUM_I2C_SDA 48
+#define EXAMPLE_PIN_NUM_I2C_SCL 47
+
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ (80 * 1000 * 1000)
+
+#define EXAMPLE_PIN_NUM_LCD_DC 42
+#define EXAMPLE_PIN_NUM_LCD_RST -1
+#define EXAMPLE_PIN_NUM_LCD_CS 45
+
+#define EXAMPLE_LCD_CMD_BITS 8
+#define EXAMPLE_LCD_PARAM_BITS 8
+
+#define EXAMPLE_LCD_H_RES 240
+#define EXAMPLE_LCD_V_RES 320
+
+#define EXAMPLE_PIN_NUM_BK_LIGHT 1
+
+#define LCD_BL_LEDC_TIMER LEDC_TIMER_0
+#define LCD_BL_LEDC_MODE LEDC_LOW_SPEED_MODE
+
+#define LCD_BL_LEDC_CHANNEL LEDC_CHANNEL_0
+#define LCD_BL_LEDC_DUTY_RES LEDC_TIMER_10_BIT // Set duty resolution to 13 bits
+#define LCD_BL_LEDC_DUTY (1024)                // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LCD_BL_LEDC_FREQUENCY (10000)          // Frequency in Hertz. Set frequency at 5 kHz
+
+#define EXAMPLE_LVGL_TICK_PERIOD_MS 2
+#define EXAMPLE_LVGL_TASK_MAX_DELAY_MS 500
+#define EXAMPLE_LVGL_TASK_MIN_DELAY_MS 1
+
+// 外部变量声明
+extern lv_disp_drv_t disp_drv;   /*Descriptor of a display driver*/
+extern esp_lcd_panel_handle_t panel_handle;
+extern esp_lcd_touch_handle_t tp;
+
+// 函数声明
+void display_init(void);
+void touch_init(void);
+void lv_port_disp_init(void);
+void lv_port_indev_init(void);
+void bsp_brightness_init(void);
+void bsp_brightness_set_level(uint8_t level);
+void lvgl_tick_timer_init(uint32_t ms);
+bool lvgl_lock(int timeout_ms);
+void lvgl_unlock(void);
+
+// 相机相关函数
+void camera_init(void);
+void downscale_rgb565(
+    uint16_t *src, int src_w, int src_h,
+    uint16_t *dst, int dst_w, int dst_h);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // INIT_BOARD_H
